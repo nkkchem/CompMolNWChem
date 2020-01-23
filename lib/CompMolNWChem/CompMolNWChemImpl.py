cf47658c7562a40af97dc729006ccdceb7e01123
mcnaughtonadm <mcna892@we28854.pnl.gov>
2020-01-22 22:06:19 -0800

    Updates to Impl

--- lib/CompMolNWChem/CompMolNWChemImpl.py
+++ lib/CompMolNWChem/CompMolNWChemImpl.py
@@ -5,6 +5,13 @@ import os
 import sys
 import subprocess as _subprocess
 import csv
+import inchi_to_submission as its
+import extract_properties_mulliken_charges_mol2 as mul
+import compound_parsing as com
+import pandas as pd
+import compound_parsing as parse
+import export as ex
+
 from pybel import *
 from rdkit import Chem
 from rdkit.Chem import AllChem
@@ -12,9 +19,11 @@ from rdkit.Chem import Descriptors
 from csv import DictReader
 from installed_clients.KBaseReportClient import KBaseReport
 from installed_clients.DataFileUtilClient import DataFileUtil
-from installed_clients.CompoundSetUtilsClient import CompoundSetUtils
-import compound_parsing as parse
-import export as ex
+
+#from installed_clients.CompoundSetUtilsClient import CompoundSetUtils
+from CompoundSetUtil import CompoundSetUtils
+
+
 #END_HEADER
 
 
@@ -38,6 +47,7 @@ class CompMolNWChem:
     GIT_COMMIT_HASH = ""
 
     #BEGIN_CLASS_HEADER
+    @staticmethod
     def _check_param(in_params, req_param, opt_param=list()):
         """
         Check if each of the params in the list are in the input params
@@ -131,6 +141,53 @@ class CompMolNWChem:
 
         return packed_mol2_files_path, comp_id_mol2_file_name_map
 
+    def _covert_mol2_files_to_pdbqt(self, ref):
+        compoundset_obj = self.dfu.get_objects(
+            {'object_refs': [ref]}
+        )['data'][0]
+        compoundset_info = compoundset_obj['info']
+        compoundset = compoundset_obj['data']
+        mol2_temp_dir = "{}/{}".format(self.scratch, uuid.uuid4())
+        os.mkdir(mol2_temp_dir)
+        pdbqt_temp_dir = "{}/{}".format(self.scratch, uuid.uuid4())
+        os.mkdir(pdbqt_temp_dir)
+
+        compounds = compoundset.get('compounds')
+
+        pdbqt_files = []
+        comp_id_pdbqt_file_name_map = {}
+        for compound in compounds:
+            mol2_handle_ref = compound.get('mol2_handle_ref')
+
+            if mol2_handle_ref:
+                mol2_file_path = self.dfu.shock_to_file(
+                                            {'handle_id': mol2_handle_ref,
+                                             'file_path': mol2_temp_dir}).get('file_path')
+                pdbqt_file_path = os.path.join(pdbqt_temp_dir, compound['id'] + '.pdbqt')
+
+                command = ['obabel', '-i', 'mol2', mol2_file_path, '-o', 'pdbqt', '-O', pdbqt_file_path]
+                process = Popen(command, stdout=PIPE, stderr=PIPE)
+                stdout, stderr = process.communicate()
+
+                if 'converted' in str(stderr) and 'molecule' in str(stderr):
+                    logging.info('Successfully converted Mol2 to pdbqt format: {}'.format(
+                                                            os.path.basename(mol2_file_path)))
+                    pdbqt_files.append(pdbqt_file_path)
+                    comp_id_pdbqt_file_name_map[compound['id']] = os.path.basename(
+                                                                            pdbqt_file_path)
+                else:
+                    logging.warning('Cannot convert Mol2 file to pdbqt format: {}'.format(
+                                                            os.path.basename(mol2_file_path)))
+                    logging.warning(stderr)
+
+        packed_pdbqt_files_path = None
+        if pdbqt_files:
+            packed_pdbqt_files_path = os.path.join(pdbqt_temp_dir, compoundset_info[1] + '_pdbqt_files.zip')
+            with zipfile.ZipFile(packed_pdbqt_files_path, 'w') as zipMe:
+                for file in pdbqt_files:
+                    zipMe.write(file, compress_type=zipfile.ZIP_DEFLATED)
+
+        return packed_pdbqt_files_path, comp_id_pdbqt_file_name_map
     #END_CLASS_HEADER
 
     # config contains contents of config file in a hash or None if it couldn't
@@ -138,12 +195,12 @@ class CompMolNWChem:
     def __init__(self, config):
         #BEGIN_CONSTRUCTOR
         self.callback_url = os.environ['SDK_CALLBACK_URL']
+        self.dfu = DataFileUtil(self.callback_url)
         self.shared_folder = config['scratch']
         logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
                             level=logging.INFO)
         #END_CONSTRUCTOR
         pass
-
 
     def run_CompMolNWChem(self, ctx, params):
         """
@@ -157,20 +214,21 @@ class CompMolNWChem:
         #BEGIN run_CompMolNWChem
 
         sim_dir = '~/../simulation'
-        os.system('ls')
-        import pandas as pd
+        
+       
         
         # Read inputs from .tsv file
 
-        df = CompoundSetUtils.compound_set_from_file(self,ctx,params['Input_File'])
         
+        df = CompoundSetUtils.compound_set_from_file(self,ctx,{'workspace_id':params['workspace_name'],
+                                                               'staging_file_path':self.shared_folder,
+                                                               'compound_set_name':params['Input_File']})
+
+        exit()
         #df = pd.read_csv(params['Input_File'], sep ='\t')
         ids = df['id']
         InChIes = df['structure']
 
-        import inchi_to_submission as its
-        import extract_properties_mulliken_charges_mol2 as mul
-        import compound_parsing as com
         
         its.inchi_to_dft(ids,InChIes)
 
@@ -188,22 +246,6 @@ class CompMolNWChem:
 
             mul.calculate(ids[i])
 
-        #for j in range(length):
-        #    os.chdir('./'+ids[j]+'/dft')
-        #    os.system('ls')
-
-        #    with open(ids[j]+'_Mulliken.mol2') as IN:
-                
-            
-        #        AllChem.MolToSmiles(IN,True)
-            #xx = com._make_compound_info(open(ids[j]+'_Mulliken.mol2'))
-            #print(xx)
-                
-            #os.chdir('../..')
-
-        #
-        #xx = ex._save_to_ws_and_report(self,params['workspace_name'],'~/../../simulation','compoundset')
-        
         report = KBaseReport(self.callback_url)
         report_info = report.create({'report':{'objects_created':[],
                                                'text_message': params['Input_File'],
