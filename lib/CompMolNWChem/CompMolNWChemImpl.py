ac9d0287d5c1e903785faac8dc4a7d278def7290
mcnaughtonadm <mcna892@we28854.pnl.gov>
2020-02-20 09:28:58 -0800

    Correct Changes to Output

--- lib/CompMolNWChem/CompMolNWChemImpl.py
+++ lib/CompMolNWChem/CompMolNWChemImpl.py
@@ -274,12 +274,12 @@ class CompMolNWChem:
                
                
         if count:
-            message = 'Successfully fetched {} Mol2 files from Scratch Directory'.format(count)
+            message = 'Successfully fetched {} Mol2 files from Staging Path'.format(count)
 
 
         ## Create Extended Report
 
-        output_files = self._generate_output_file_list(result_directory)
+        output_files = self._generate_output_file_list(mol2_file_path)
 
 
         report_params = {'message': message,
