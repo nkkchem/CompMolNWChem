c1246fcd04b8e80b3b467d1de0d120e8e9a67785
mcnaughtonadm <mcna892@we39314.pnl.gov>
2020-08-31 13:25:38 -0700

    Redid csv2inchi

--- lib/CompMolNWChem/CompMolNWChemImpl.py
+++ lib/CompMolNWChem/CompMolNWChemImpl.py
@@ -147,44 +147,6 @@ class CompMolNWChem:
                   'compoundset_ref': compoundset_ref}
         return output
 
-    def _csv2inchi(inchilist):
-
-        with open(inchilist,'r') as f:
-            if 'test.csv' in inchilist:
-                reader = csv.reader(f,delimiter=(','))
-
-            lineCount = 0
-            for row in reader:
-                if lineCount == 0:
-                    InChIKey = row.index("InChI-Key")
-                    InChI = row.index("InChI")
-                    lineCount+=1
-                else:
-                    print(row)
-                    inchikey_str=row[InChIKey].split('=')[1]
-                    print('inchikey string ', inchikey_str)
-
-                    moldir = inchikey_str
-                    if not os.path.exists(moldir):
-                        os.mkdir(moldir)
-
-
-                    initial_structure_dir = moldir + '/initial_structure'
-                    if not os.path.exists(initial_structure_dir):
-                        os.mkdir(initial_structure_dir)
-
-                        md_structure_dir = moldir + '/md'
-                        if not os.path.exists(md_structure_dir):
-                            os.mkdir(md_structure_dir)
-
-                        dft_structure_dir = moldir + '/dft'
-                        if not os.path.exists(dft_structure_dir):
-                            os.mkdir(dft_structure_dir)
-
-                        inchifile_str = initial_structure_dir + '/' + inchikey_str + '.inchi'
-                        with open(inchifile_str,'w+') as f:
-                            f.write(row[InChI])
-                            
     #END_CLASS_HEADER
 
     # config contains contents of config file in a hash or None if it couldn't
@@ -298,10 +260,46 @@ class CompMolNWChem:
 
 #            mul.calculate(ids[i])
 
-        self._csv2inchi("test.csv")
-        print('Here')
-
+       
         from snakemake import snakemake
+
+        inchilist = params['Input_File']
+        
+        with open(inchilist,'r') as f:
+            if 'inchi.csv' in inchilist:
+                reader = csv.reader(f,delimiter=(','))
+
+	        lineCount = 0
+	        for row in reader:
+                    if lineCount == 0:
+		        InChIKey = row.index("InChI-Key")
+		        InChI = row.index("InChI")
+		        lineCount+=1
+                    else:
+                        print(row)
+		        inchikey_str=row[InChIKey].split('=')[1]
+		        print('inchikey string ', inchikey_str)
+                        
+		        moldir = inchikey_str
+		        if not os.path.exists(moldir):
+                            os.mkdir(moldir)
+
+                        
+		        initial_structure_dir = moldir + '/initial_structure'
+		        if not os.path.exists(initial_structure_dir):
+			    os.mkdir(initial_structure_dir)
+
+		        md_structure_dir = moldir + '/md'
+                        if not os.path.exists(md_structure_dir):
+			    os.mkdir(md_structure_dir)
+
+		        dft_structure_dir = moldir + '/dft'
+		        if not os.path.exists(dft_structure_dir):
+			    os.mkdir(dft_structure_dir)
+
+		        inchifile_str = initial_structure_dir + '/' + inchikey_str + '.inchi'
+		        with open(inchifile_str,'w+') as f:
+                            f.write(row[InChI])
 
         os.system('snakemake -p --cores 2 --snakefile snakemake/final_pipeline.snakemake -w 300')
 
