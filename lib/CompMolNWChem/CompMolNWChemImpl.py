b5d199c8704e18e8a92cb9d8f30c846815d183df
mcnaughtonadm <mcna892@we39314.pnl.gov>
2020-08-31 10:23:31 -0700

    Impl Fix

--- lib/CompMolNWChem/CompMolNWChemImpl.py
+++ lib/CompMolNWChem/CompMolNWChemImpl.py
@@ -5,8 +5,8 @@ import os
 import sys
 import subprocess as _subprocess
 import csv
-#import inchi_to_submission as its
-#import extract_properties_mulliken_charges_mol2 as mul
+import inchi_to_submission as its
+import extract_properties_mulliken_charges_mol2 as mul
 import compound_parsing as com
 import pandas as pd
 import compound_parsing as parse
@@ -223,40 +223,9 @@ class CompMolNWChem:
         for d in compounds:
            ids.append(d['id'])
            smiles.append(d['smiles'])
-        print(ids)
-        print(smiles)
+        #print(ids)
+        #print(smiles)
         
-        linecount = 0
-        for row in compounds:
-            if linecount == 0:
-                InChIKey = row.index("InChI-Key")
-                InChI = row.index("InChI")
-                linecount+=1
-            else:
-                print(row)
-                inchikey_str=row[InChIKey].split('=')[1]
-                print('inchikey string ', inchikey_str)
-
-                moldir = inchikey_str
-                if not os.path.exists(moldir):
-                    os.mkdir(moldir)
-
-                initial_structure_dir = moldir + '/initial_structure'
-                if not os.path.exists(initial_structure_dir):
-                    os.mkdir(initial_structure_dir)
-
-                md_structure_dir = moldir + '/md'
-                if not os.path.exists(md_structure_dir):
-                    os.mkdir(md_structure_dir)
-
-                dft_structure_dir = moldir + '/dft'
-                if not os.path.exists(dft_structure_dir):
-                    os.mkdir(dft_structure_dir)
-
-                inchifile_str = initial_structure_dir + '/' + inchikey_str + '.inchi'
-                with open(inchifile_str, 'w+') as f:
-                    f.write(row[InChi])
-
         #os.system('snakemake -p --cores 2 --snakefile snakemake/final_pipeline.snakemake -w 300')
         
         # Read the ids and structures of the compounds
