f8ebc028afa4e07d365991f6938dc8c7656186fc
mcnaughtonadm <mcna892@we39314.pnl.gov>
2020-08-31 13:45:39 -0700

    Hotfix 7

--- lib/CompMolNWChem/CompMolNWChemImpl.py
+++ lib/CompMolNWChem/CompMolNWChemImpl.py
@@ -283,22 +283,21 @@ class CompMolNWChem:
                     moldir = inchikey_str
                     if not os.path.exists(moldir):
                         os.mkdir(moldir)
+    
+                    initial_structure_dir = moldir + '/initial_structure'
+                    if not os.path.exists(initial_structure_dir):
+                        os.mkdir(initial_structure_dir)
 
-                        
-		    initial_structure_dir = moldir + '/initial_structure'
-		    if not os.path.exists(initial_structure_dir):
-			os.mkdir(initial_structure_dir)
-
-		    md_structure_dir = moldir + '/md'
+                    md_structure_dir = moldir + '/md'
                     if not os.path.exists(md_structure_dir):
-			os.mkdir(md_structure_dir)
+                        os.mkdir(md_structure_dir)
 
-		    dft_structure_dir = moldir + '/dft'
-		    if not os.path.exists(dft_structure_dir):
-			os.mkdir(dft_structure_dir)
+                    dft_structure_dir = moldir + '/dft'
+                    if not os.path.exists(dft_structure_dir):
+                        os.mkdir(dft_structure_dir)
 
-		    inchifile_str = initial_structure_dir + '/' + inchikey_str + '.inchi'
-		    with open(inchifile_str,'w+') as f:
+                    inchifile_str = initial_structure_dir + '/' + inchikey_str + '.inchi'
+                    with open(inchifile_str,'w+') as f:
                         f.write(row[InChI])
 
         os.system('snakemake -p --cores 2 --snakefile snakemake/final_pipeline.snakemake -w 300')
