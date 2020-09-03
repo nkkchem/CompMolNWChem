f33f367698bdbd062628686f6fca6344062a74a7
mcnaughtonadm <mcna892@we39314.pnl.gov>
2020-09-03 11:41:16 -0700

    Updated Workflow for Modelseed

--- lib/CompMolNWChem/CompMolNWChemImpl.py
+++ lib/CompMolNWChem/CompMolNWChemImpl.py
@@ -263,44 +263,53 @@ class CompMolNWChem:
        
         from snakemake import snakemake
 
-        inchilist = scratch_file_path
-        
-        with open(inchilist,'r') as f:
-#            if 'inchi.csv' in inchilist:
-            reader = csv.reader(f,delimiter=(','))
+        reactionlist = scratch_file_path
 
-            lineCount = 0
-            for row in reader:
-                if lineCount == 0:
-                    InChIKey = row.index("InChI-Key")
-                    InChI = row.index("InChI")
-                    lineCount+=1
-                else:
-                    print(row)
-                    inchikey_str=row[InChIKey].split('=')[1]
-                    print('inchikey string ', inchikey_str)
+        id_to_smiles = {}
+        data = open('/kb/module/modelseed_test.csv','r')
 
-                    moldir = inchikey_str
-                    if not os.path.exists(moldir):
-                        os.mkdir(moldir)
+        for lines in data.readlines():
+            id = lines.split(',')[0]
+            smiles = lines.split(',')[1].rstrip()
+            id_to_smiles[id] = smiles
+
+        data.close()
+
+        with open(reactionlist,'r') as f:
+            reactions = f.readlines()[0].rstrip()
+            reactant = reactions.split('=')[0].split('+')
+            product = reactions.split('=')[1].split('+')
+            metabolites = []
+            for each in reactant:
+                each = each.strip()
+                metabolites.append(each)
+            for each in product:
+                each = each.strip()
+                metabolites.append(each)
+
+            for molecule in metabolites:
+                
+                moldir = molecule
+                if not os.path.exists(moldir):
+                    os.mkdir(moldir)
     
-                    initial_structure_dir = moldir + '/initial_structure'
-                    if not os.path.exists(initial_structure_dir):
-                        os.mkdir(initial_structure_dir)
+                initial_structure_dir = moldir + '/initial_structure'
+                if not os.path.exists(initial_structure_dir):
+                    os.mkdir(initial_structure_dir)
 
-                    md_structure_dir = moldir + '/md'
-                    if not os.path.exists(md_structure_dir):
-                        os.mkdir(md_structure_dir)
+                md_structure_dir = moldir + '/md'
+                if not os.path.exists(md_structure_dir):
+                    os.mkdir(md_structure_dir)
 
-                    dft_structure_dir = moldir + '/dft'
-                    if not os.path.exists(dft_structure_dir):
-                        os.mkdir(dft_structure_dir)
+                dft_structure_dir = moldir + '/dft'
+                if not os.path.exists(dft_structure_dir):
+                    os.mkdir(dft_structure_dir)
 
-                    inchifile_str = initial_structure_dir + '/' + inchikey_str + '.inchi'
-                    with open(inchifile_str,'w+') as f:
-                        f.write(row[InChI])
+                inchifile_str = initial_structure_dir + '/' + moldir + '.smiles'
+                with open(inchifile_str,'w+') as f:
+                    f.write(id_to_smiles[moldir])
         
-        os.system('snakemake -p --cores 2 --snakefile snakemake-scripts/final_pipeline.snakemake -w 300')
+        os.system('snakemake -p --cores 3 --snakefile snakemake-scripts/final_pipeline.snakemake -w 12000')
 
         # Build KBase Output. Should output entire /simulation directory and build a CompoundSet with Mol2 Files
 
--- nwchem-scripts/create_nw_files.py
+++ nwchem-scripts/create_nw_files.py
@@ -1,10 +1,14 @@
 import os
 import sys
 
+
+symbol_to_atomic_num = {'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C':6, 'N': 7, 'O':8, 'F':9, 'Ne': 10, 'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16, 'Cl':17,'Ar': 18, 'K': 19, 'Ca':20, 'Sc': 21, 'Ti': 22, 'V':23, 'Cr':24, 'Mn':25, 'Fe':26, 'Co':27, 'Ni':28, 'Cu':29, 'Zn':30, 'Ga': 31, 'Ge':32, 'As':33, 'Se':34, 'Br':35, 'Kr':36, 'Rb':37, 'Sr':38, 'Y':39, 'Zr':40, 'Nb':41, 'Mo':42, 'Tc':43, 'Ru':44, 'Rh':45, 'Pd':46, 'Ag':47, 'Cd':48, 'In':49, 'Sn':50}
+
 def dft_nw_file(InChI_key):
     dft_dir    =  InChI_key + '/' + 'dft' 
     nwfile     = InChI_key +'.nw'
     chargefile = InChI_key +'.charge'
+    xyzfile    = InChI_key +'.xyz'
 
     os.chdir(dft_dir)
     path    =  os.getcwd()
@@ -17,7 +21,25 @@ def dft_nw_file(InChI_key):
               for i in chgfile.readlines():
                   CHARGE = i.split('\t')[1]
 #
-         lines = lines.replace('CHARGE', '-1')
+         lines = lines.replace('CHARGE', CHARGE)
+
+#
+
+    with open(xyzfile, 'r') as xyz:
+         natom = xyz.readline()
+         comment = xyz.readline()
+         sum_elctrons = 0
+         for i in range(0, int(natom.rstrip())):
+             symbol = xyz.readline().split()[0]
+             sum_elctrons += float(symbol_to_atomic_num[symbol])
+
+         total_elctrons = sum_elctrons + float(CHARGE)
+         if total_elctrons % 2 == 0:
+             lines = lines.replace('multiplicity', '3')
+         else:
+             lines = lines.replace('multiplicity', '2 \n  odft')
+
+#
 
     with open(nwfile, 'w') as file:
          file.write(lines)
@@ -41,7 +63,5 @@ if __name__ == '__main__':
     InChI_key = sys.argv[1]
     dft_nw_file(InChI_key)
     dft_sbatch_file(InChI_key)
-
-
 
 
